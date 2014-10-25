4ba5c0d0be9906e8950c28bd8f4945d4b4344e13
kobake <kobake@users.sourceforge.net>
2014-10-25 10:07:27 +0900

    HTML parse by Jsoup

--- src/com/example/pi08uselib/MainActivity.java
+++ src/com/example/pi08uselib/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.pi08uselib;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +29,30 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void buttonMethod(View button){
+		String msg = "";
+		
+		// サンプルHTML
+		String html = "<a href='xyz'>hoge</a>\n<a href='google'>piyo</a>";
+		
+		// HTMLパース
+		Document doc = Jsoup.parse(html);
+		Elements links = doc.select("a");
+		for(int i = 0; i < links.size(); i++){
+			try{
+				Element link = links.get(i);
+				String url = link.attr("href");
+				//URL urlObj = new URL(baseUrl, url);
+				// 結果追記
+				msg += url + "\n";
+			}
+			catch(Exception ex){
+				msg += "Error: " + ex.toString() + "\n";
+			}
+		}
+		
+		// 表示
+		TextView text = (TextView)findViewById(R.id.textView1);
+		text.setText(msg);
+	}
 }
